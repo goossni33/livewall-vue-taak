a8c8e396dbc0e3af7254ecdbf8ef45db518ad722
NickGoossens <nickgoossens@livewallgroup.com>
2021-01-25 17:46:26 +0100

    [INIT] init push

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div class="nav">
+      <div class="full-width">
+        <router-link to="/" class="link">Home</router-link>
+        <router-link to="/about" class="link">About</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .nav {
+    background-color: cadetblue;
 
-#nav {
-  padding: 30px;
+    .full-width {
+      display: flex;
+      align-items: center;
+    }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    .link {
+      margin: 0 12px;
+      color: white;
+      text-decoration: none;
     }
   }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,152 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="full-width">
+      <h1>Home</h1>
+      <h2 v-if="participants.length !== 0">Welkom {{participants[participants.length - 1].name}} bij de cursus.</h2>
+      <div class="participants-list">
+        <div v-for="(participant, index) in participants" :key="index" class="list-item">
+          <div class="body" v-if="!participant.editable">
+            <h3>{{participant.name}}</h3>
+            <p>{{participant.team}}</p>
+          </div>
+          <div class="body" v-else>
+            <input type="text" v-model="participant.name">
+            <input type="text" v-model="participant.team">
+          </div>
+          <div class="button-container">
+            <div @click="toggleEditable(index)" class="button edit circular" :class="{ confirm: participant.editable }"></div>
+            <div @click="deleteItem(index)" class="button delete circular"></div>
+          </div>
+        </div>
+      </div>
+      <form>
+        <h2>Voeg een deelnemer toe:</h2>
+        <input type="text" v-model="form.name" placeholder="Naam">
+        <input type="text" v-model="form.team" name="team" placeholder="Team">
+        <button @click="onSubmit">Toevoegen</button>
+      </form>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style lang="scss">
+  .list-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 12px 0;
+    border-bottom: 1px solid grey;
 
+    &:hover {
+      .button-container {
+        display: flex;
+      }
+    }
+  }
+
+  form {
+    input[type=text] {
+      margin-bottom: 8px;
+    }
+  }
+
+  .button-container {
+    display: none;
+    align-items: center;
+
+    .button {
+      display: block;
+      margin-left: 8px;
+      cursor: pointer;
+
+      &.circular {
+        width: 24px;
+        height: 24px;
+
+        border-radius: 100%;
+      }
+
+      &.delete {
+        background-color: #e74c3c;
+        background-image: url('~@/assets/images/icons/cancel.svg');
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+
+      &.edit {
+        border: 1px solid grey;
+        background-size: 16px;
+        background-image: url('~@/assets/images/icons/edit.svg');
+        background-position: center;
+        background-repeat: no-repeat;
+
+        &.confirm {
+          border: none;
+          background-size: 18px;
+          background-image: url('~@/assets/images/icons/check.svg');
+          background-color: #2ecc71;
+        }
+      }
+    }
+
+  }
+
+</style>
+
+<script>
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      form: {
+        name: '',
+        team: ''
+      },
+      participants: [
+        {
+          name: 'Dennis',
+          team: 'engagement',
+          editable: false
+        }, {
+          name: 'Jeffrey',
+          team: 'engagement',
+          editable: false
+        }, {
+          name: 'Pim',
+          team: 'platforms',
+          editable: false
+        }, {
+          name: 'Tom',
+          team: 'platforms',
+          editable: false
+        }
+      ]
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+
+      if(!this.form.name || !this.form.team) {
+        return false;
+      }
+
+      this.participants.push({
+        name: this.form.name,
+        team: this.form.team,
+        editable: false
+      });
+
+      this.form.name = '';
+      this.form.team = '';
+    },
+    deleteItem(index) {
+      this.participants.splice(index, 1);
+    },
+    toggleEditable(index) {
+      this.participants[index].editable = !this.participants[index].editable;
+    }
   }
 }
 </script>
