69be437b498c938f9bf7f14afa056743f1bce16a
NickGoossens <nickgoossens@livewallgroup.com>
2021-01-25 17:50:16 +0100

    [INIT] init push

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="nav">
+      <div class="full-width">
+        <router-link to="/" class="link">Home</router-link>
+        <router-link to="/about" class="link">About</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  .nav {
+    background-color: cadetblue;
+
+    .full-width {
+      display: flex;
+      align-items: center;
+    }
+
+    .link {
+      margin: 0 12px;
+      color: white;
+      text-decoration: none;
+    }
+  }
+</style>
